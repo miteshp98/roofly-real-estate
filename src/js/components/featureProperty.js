b4d5e5d7397fc10f1750180db907a6c17ead95f5
miteshp98 <miteshpanchal3d@gmail.com>
2024-12-06 19:26:32 +0530

    chore: create featureProperty component for rendering feature property

---
+++ src/js/components/featureProperty.js
@@ -0,0 +1,30 @@
+import PropertyCard from "./propertyCard";
+
+class FeatureProperty extends PropertyCard {
+  constructor() {
+    super();
+    this._featureData();
+  }
+
+  async _featureData() {
+    const featureProperty = await this._fetchEntries(0, 3, "fields.price");
+
+    if (!featureProperty) {
+      return;
+    }
+
+    this._renderFeatureProperty(featureProperty);
+  }
+
+  _renderFeatureProperty(data) {
+    const featureContainer = document.querySelector(".featured-projects");
+
+    if (!featureContainer) {
+      return;
+    }
+
+    featureContainer.innerHTML = this._renderProperties(data);
+  }
+}
+
+export const featureProperty = new FeatureProperty();
