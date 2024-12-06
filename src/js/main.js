33b9847bbe24d47f6b02bc0b37272f949fa8e668
miteshp98 <miteshpanchal3d@gmail.com>
2024-12-06 19:22:02 +0530

    chore: set up main.js

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+import { HeaderManager } from "./components/header";
+import { featureProperty } from "./components/featureProperty";
+import { latestProperties } from "./components/latestProperty";
+
+const headerManager = new HeaderManager();
