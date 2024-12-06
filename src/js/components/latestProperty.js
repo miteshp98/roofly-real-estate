73db0577ad00331ca1076078b28fccbf8df1f233
miteshp98 <miteshpanchal3d@gmail.com>
2024-12-06 19:27:16 +0530

    chore: create latest property component for rendering Latest properties

---
+++ src/js/components/latestProperty.js
@@ -0,0 +1,30 @@
+import PropertyCard from "./propertyCard";
+
+class LatestProperties extends PropertyCard {
+  constructor() {
+    super();
+    this._latestPropertyData();
+  }
+
+  async _latestPropertyData() {
+    const latestProperties = await this._fetchEntries(0, 6, "-sys.createdAt");
+
+    if (!latestProperties) {
+      return;
+    }
+
+    this._renderLatestProperties(latestProperties);
+  }
+
+  _renderLatestProperties(data) {
+    const latestPropertyContainer = document.querySelector(".properties-list");
+
+    if (!latestPropertyContainer) {
+      return;
+    }
+
+    latestPropertyContainer.innerHTML = this._renderProperties(data);
+  }
+}
+
+export const latestProperties = new LatestProperties();
