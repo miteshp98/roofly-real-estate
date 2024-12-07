f76fa6c9dbbcdde12e2b4fbb4f88a7c76f3ed044
miteshp98 <miteshpanchal3d@gmail.com>
2024-12-08 02:33:55 +0530

    chore: add testimonial carousel logic

---
+++ src/js/components/testimonialCarousel.js
@@ -0,0 +1,76 @@
+import { client } from "../utils/contentfulApi";
+import TestimonialManager from "./testimonialCard";
+
+class TestimonialCarousel extends TestimonialManager {
+  #section = document.querySelector(".testimonial-section");
+  #container = document.querySelector(".testimonial-carousel-container");
+  #wrapper = this.#container.querySelector(".testimonial-slider-wrapper");
+  #currentIndex = 0;
+
+  constructor() {
+    super();
+    this._processTestimonialData();
+  }
+
+  async _processTestimonialData() {
+    const testimonialData = await this._fetchTestimonialEntries();
+
+    this.#wrapper.innerHTML = this._renderTestimonialCard(testimonialData);
+
+    this._selectTestimonialElements(this.#wrapper.children);
+  }
+
+  _selectTestimonialElements(testimonialSlides) {
+    const slides = Array.from(testimonialSlides);
+
+    if (!slides) {
+      return;
+    }
+
+    this._updateTrackPosition(slides);
+    this._handleCarouselCTA(slides);
+  }
+
+  _updateTrackPosition(slides) {
+    const containerWidth = this.#container.clientWidth;
+    const slideWidth = slides[0].clientWidth;
+    const marginRight = 50;
+
+    const offset = this.#currentIndex * (slideWidth + marginRight);
+
+    this.#container.style.transform = `translateX(calc(${
+      containerWidth / 2
+    }px - ${slideWidth / 2}px - ${offset}px))`;
+  }
+
+  _handleCarouselCTA(slides) {
+    this.#section.addEventListener("click", (e) => {
+      const nextBtn = e.target.closest(".carousel-next-btn");
+      const prevBtn = e.target.closest(".carousel-prev-btn");
+
+      if (nextBtn) {
+        this._goToNextSlide(slides);
+      } else if (prevBtn) {
+        this._goToPrevSlide(slides);
+      }
+
+      if (!nextBtn || !prevBtn) {
+        return;
+      }
+    });
+  }
+
+  _goToNextSlide(slides) {
+    this.#currentIndex = (this.#currentIndex + 1) % slides.length;
+    this._updateTrackPosition(slides);
+  }
+
+  _goToPrevSlide(slides) {
+    this.#currentIndex =
+      (this.#currentIndex - 1 + slides.length) % slides.length;
+
+    this._updateTrackPosition(slides);
+  }
+}
+
+export const testimonialCarousel = new TestimonialCarousel();
