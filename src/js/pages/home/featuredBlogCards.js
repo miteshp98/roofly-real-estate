dd77ea783f0d9769f0e15f2618c3572594c75079
miteshp98 <miteshpanchal3d@gmail.com>
2024-12-08 02:34:36 +0530

    chore: add featureblog post display logic

---
+++ src/js/pages/home/featuredBlogCards.js
@@ -0,0 +1,30 @@
+import BlogCardManager from "../../components/blogCard";
+
+class FeaturedBlogCards extends BlogCardManager {
+  constructor() {
+    super();
+    this._featuredBlogData();
+  }
+
+  async _featuredBlogData() {
+    const featureBlogs = await this._fetchEntries(0, 2, "sys.createdAt");
+
+    if (!featureBlogs) {
+      return;
+    }
+
+    this._renderFeaturedBlogs(featureBlogs);
+  }
+
+  _renderFeaturedBlogs(data) {
+    const blogCardContainer = document.querySelector(".blog-card-container");
+
+    if (!blogCardContainer) {
+      return;
+    }
+
+    blogCardContainer.innerHTML = this._renderBlogCard(data);
+  }
+}
+
+export const featuredBlogCards = new FeaturedBlogCards();
