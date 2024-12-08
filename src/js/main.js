6b2431e046c386e25534e75b2263e7f790544113
miteshp98 <miteshpanchal3d@gmail.com>
2024-12-08 15:59:36 +0530

    chore: add carousel manage

--- src/js/main.js
+++ src/js/main.js
@@ -3,5 +3,7 @@ import { featureProperty } from "./pages/home/featureProperty";
 import { latestProperties } from "./pages/home/latestProperty";
 import { featuredBlogCards } from "./pages/home/featuredBlogCards";
 import { testimonialCarousel } from "./components/testimonialCarousel";
+import { FAQAccodionManager } from "./components/faqAccordion";
 
 const headerManager = new HeaderManager();
+const faqAccordion = new FAQAccodionManager();
