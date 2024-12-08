e06bc6315656a6deb00452fc0cb6902739ca7eb7
miteshp98 <miteshpanchal3d@gmail.com>
2024-12-08 22:42:32 +0530

    import new component logic

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ import { latestProperties } from "./pages/home/latestProperty";
 import { featuredBlogCards } from "./pages/home/featuredBlogCards";
 import { testimonialCarousel } from "./components/testimonialCarousel";
 import { FAQAccodionManager } from "./components/faqAccordion";
+import { properties } from "./pages/properties/propertiesList";
 
 const headerManager = new HeaderManager();
 const faqAccordion = new FAQAccodionManager();
