a95e30f27d02cba0ec64ba1c7e781d5c5b7562f1
miteshp98 <miteshpanchal3d@gmail.com>
2024-12-09 16:12:48 +0530

    add display services logic

---
+++ src/js/pages/about/featureServiceView.js
@@ -0,0 +1,28 @@
+import ServicesManager from "../../components/servicesManager";
+
+class FeatureServiceView extends ServicesManager {
+  constructor() {
+    super();
+    this._fetchFeatureServices();
+  }
+
+  async _fetchFeatureServices() {
+    const servicesData = await this._fetchentries(0, 4, "sys.createdAt");
+
+    if (!servicesData) {
+      return;
+    }
+
+    this._renderFeatureServices(servicesData);
+  }
+
+  _renderFeatureServices(data) {
+    const parentElement = document.querySelector(".services-content-wrap");
+
+    if (!parentElement) return;
+
+    parentElement.innerHTML = this._renderServicesMarkup(data);
+  }
+}
+
+export const featureServiceView = new FeatureServiceView();
