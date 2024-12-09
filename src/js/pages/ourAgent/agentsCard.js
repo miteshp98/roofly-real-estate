7aa4d5974517b08a7a3363f91abc73f4a4d34c04
miteshp98 <miteshpanchal3d@gmail.com>
2024-12-09 17:02:16 +0530

    add agents card view for display agents cards

---
+++ src/js/pages/ourAgent/agentsCard.js
@@ -0,0 +1,25 @@
+import AgentCardManager from "../../components/agentsCardManager";
+
+class agentCardView extends AgentCardManager {
+  constructor() {
+    super();
+
+    this._fetchAgenData();
+  }
+
+  async _fetchAgenData() {
+    const entries = await this._fetchEnries();
+
+    this._displayAgentCards(entries);
+  }
+
+  _displayAgentCards(data) {
+    const container = document.querySelector(".agent-card-container");
+
+    if (!container) return;
+
+    container.innerHTML = this._renderAgentCards(data);
+  }
+}
+
+const agentCards = new agentCardView();
