615f64eb1fdd74e8965d242972cc658acdc47d5e
miteshp98 <miteshpanchal3d@gmail.com>
2024-12-09 16:16:25 +0530

    import new files

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@ import { latestProperties } from "./pages/home/latestProperty";
 import { featuredBlogCards } from "./pages/home/featuredBlogCards";
 import { testimonialCarousel } from "./components/testimonialCarousel";
 import { FAQAccodionManager } from "./components/faqAccordion";
-import { properties } from "./pages/properties/propertiesList";
+import { featureServiceView } from "./pages/about/featureServiceView";
 
 const headerManager = new HeaderManager();
 const faqAccordion = new FAQAccodionManager();
